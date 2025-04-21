e43c6af7a4f27f4e7d594e83f74d99ff856cc732
Diego Roccia <diego.roccia@gmail.com>
2025-04-21 18:21:09 +0200

    changes from ZALANDO-840211 on Mon Apr 21 06:21:09 PM CEST 2025

--- lua/plugins/zalando.lua
+++ lua/plugins/zalando.lua
@@ -3,6 +3,7 @@ return {
         "diego-roccia_zse/zalando.nvim",
         -- url = "https://github.com/diego-roccia_zse/zalando.nvim",
         config = {
+            organization = "zalando-build",
             topic = "linus"
         },
     }
