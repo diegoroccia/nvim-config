a7639a24cbe1af74e360f3aa356f81192d15a33c
Diego Roccia <diego.roccia@gmail.com>
2025-04-21 23:31:40 +0200

    changes from ZALANDO-840211 on Mon Apr 21 11:31:40 PM CEST 2025

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,13 +6,14 @@ return {
         opts = require('config.snacks'),
         keys = {
             -- Top Pickers & Explorer
-            { "<leader><space>", function() Snacks.picker.smart() end,           desc = "Smart Find Files" },
-            { "<leader>,",       function() Snacks.picker.buffers() end,         desc = "Buffers" },
-            { "<leader>/",       function() Snacks.picker.grep() end,            desc = "Grep" },
-            { "<leader>:",       function() Snacks.picker.command_history() end, desc = "Command History" },
-            { "<leader>n",       function() Snacks.picker.notifications() end,   desc = "Notification History" },
-            { "<leader>f",       function() Snacks.explorer() end,               desc = "File Explorer" },
-            { "<C-e>",           function() Snacks.picker.icons() end,           desc = "Emoji Explorer",      mode = { "i" }, },
+            { "<leader><space>", function() Snacks.picker.smart() end,                             desc = "Smart Find Files" },
+            { "<leader>,",       function() Snacks.picker.buffers() end,                           desc = "Buffers" },
+            { "<leader>/",       function() Snacks.picker.grep() end,                              desc = "Grep" },
+            { "<leader>:",       function() Snacks.picker.command_history() end,                   desc = "Command History" },
+            { "<leader>n",       function() Snacks.picker.notifications() end,                     desc = "Notification History" },
+            { "<leader>f",       function() Snacks.explorer({ cwd = vim.fs.root(0, '.git') }) end, desc = "File Explorer" },
+
+            { "<C-e>",           function() Snacks.picker.icons() end,                             desc = "Emoji Explorer",      mode = { "i" }, },
         }
     },
 }
