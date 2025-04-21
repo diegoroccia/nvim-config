18db6a4f912a576d963012980d2153ec905bd41a
Diego Roccia <diego.roccia@gmail.com>
2025-04-21 16:29:52 +0200

    changes from ZALANDO-840211 on Mon Apr 21 04:29:52 PM CEST 2025

--- lua/plugins/zalando.lua
+++ lua/plugins/zalando.lua
@@ -3,8 +3,6 @@ return {
         "diego-roccia_zse/zalando.nvim",
         -- url = "git@github-zse:diego-roccia_zse/zalando.nvim.git",
         dir = "~/code/zalando-personal/zalando.nvim",
-        opts = {
-            topic = "teapot"
-        }
+        config = true,
     }
 }
