7842a6139c234121414730f81d7ca3f2a58f302e
Diego Roccia <diego.roccia@gmail.com>
2025-04-21 19:57:21 +0200

    changes from ZALANDO-840211 on Mon Apr 21 07:57:21 PM CEST 2025

--- after/ftplugin/yaml.lua
+++ after/ftplugin/yaml.lua
@@ -4,6 +4,3 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
-
-vim.api.nvim_set_hl(0, "@account_id.yaml", { link = "Error" })
-vim.api.nvim_set_hl(0, "@values.yaml", { link = "Identifier" })
